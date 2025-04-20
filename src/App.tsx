c1dc987d9190df3665556dc0d139128c61405b13
Adri-ESM <anggivmorales@gmail.com>
2025-04-20 15:09:49 -0400

    Initial setup: React + TypeScript + Ant Design

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd'
+
+function App () {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Bienvenida a Snack Shop 🍪</h1>
+      <Button type='primary'>Comprar snack</Button>
+    </div>
+  )
+}
+
+export default App
